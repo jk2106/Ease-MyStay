216882156823c8f1dc3c138587f0f4eb0c4d7fa2
Harshita-Kohli <54809528+Harshita-Kohli@users.noreply.github.com>
2023-10-30 14:54:59 +0530

    Hotel dao, hotel owner dao and services, controller, Thymeleaf templates

---
+++ EaseMyStay2/src/main/java/com/fil/easemystay/controllers/HotelOwnerController.java
@@ -0,0 +1,68 @@
+package com.fil.easemystay.controllers;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.fil.easemystay.entities.HotelOwner;
+import com.fil.easemystay.service.HotelOwnerService;
+
+@RequestMapping("api/v1/")
+@Controller
+public class HotelOwnerController {
+
+	@Autowired
+	private HotelOwnerService hotelOwnerService;
+
+	
+	@GetMapping("/homeOwner")
+//	@ResponseBody
+	public String home() {
+		System.out.println("inside HotelOwner home controller");
+		return "index"; //when we don't write ResponseBody, we need to specify te
+	}
+	
+	@GetMapping("/hotelOwners")
+	public List<HotelOwner> getHotelOwners() {
+		System.out.println("inside get");
+		return this.hotelOwnerService.getHotelOwners();
+	}
+
+	@GetMapping("/hotelOwner/{id}")
+	public HotelOwner getHotel(@PathVariable int id) {
+		return this.hotelOwnerService.getHotelOwner(id);
+	}
+
+	@PostMapping("/hotelOwner")
+	public HotelOwner addHotel(@RequestBody HotelOwner hotelOwner) {
+		System.out.println("inside post");
+		return this.hotelOwnerService.addHotelOwner(hotelOwner);
+	}
+
+	@PutMapping("/hotelOwner")
+	public HotelOwner updateHotel(@RequestBody HotelOwner hotelOwner) {
+		return this.hotelOwnerService.updateHotelOwner(hotelOwner);
+	}
+
+	@DeleteMapping("/hotelOwner/{hotelOwner_id}")
+	public ResponseEntity<HttpStatus> deleteHotelOwner(@PathVariable int id) {
+		try {
+			this.hotelOwnerService.removeHotelOwner(id);
+			return new ResponseEntity<>(HttpStatus.OK);
+		} catch (Exception e) {
+			return new ResponseEntity<>(HttpStatus.INTERNAL_SERVER_ERROR);
+		}
+	}
+
+}
---
+++ EaseMyStay2/src/test/java/com/fil/easemystay/EaseMyStayApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fil.easemystay;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EaseMyStayApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
