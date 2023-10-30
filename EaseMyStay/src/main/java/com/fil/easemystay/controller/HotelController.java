a6de285beae0868abd907af99f742e7c6bccb573
ashish dash <1941017024.a.ashishdash@gmail.com>
2023-10-30 14:21:44 +0530

    Hotel and Room controller and serviceImpl added

--- EaseMyStay/src/main/java/com/fil/easemystay/controller/HotelController.java
+++ EaseMyStay/src/main/java/com/fil/easemystay/controller/HotelController.java
@@ -1,5 +1,4 @@
 package com.fil.easemystay.controller;
-
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -14,38 +13,80 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.fil.easemystay.entities.Hotel;
 import com.fil.easemystay.entities.Room;
+import com.fil.easemystay.service.HotelService;
 import com.fil.easemystay.service.RoomService;
 
 @RestController
-@RequestMapping("/api/v1/hotel")
-public class RoomController {
+@RequestMapping("api/v1/")
+public class HotelController {
 
 	@Autowired
+	private HotelService hotelService;
+	@Autowired
 	private RoomService roomService;
+
+	@GetMapping("/home")
+	public String home() {
+		return "Hello World";
+	}
+
+	@GetMapping("/hotels")
+	public List<Hotel> getHotels() {
+		System.out.println("inside get");
+		return this.hotelService.getHotels();
+	}
+
+	@GetMapping("/hotel/{id}")
+	public Hotel getHotel(@PathVariable int id) {
+		return this.hotelService.getHotel(id);
+	}
+
+	@PostMapping("/hotel")
+	public Hotel addHotel(@RequestBody Hotel hotel) {
+		System.out.println("inside post");
+		return this.hotelService.addHotel(hotel);
+	}
+
+	@PutMapping("/hotel")
+	public Hotel updateHotel(@RequestBody Hotel hotel) {
+		return this.hotelService.updateHotel(hotel);
+	}
+
+	@DeleteMapping("/hotel/{id}")
+	public ResponseEntity<HttpStatus> deleteHotel(@PathVariable int id) {
+		try {
+			
+			this.hotelService.removeHotel(id);
+			return new ResponseEntity<>(HttpStatus.OK);
+		} catch (Exception e) {
+			return new ResponseEntity<>(HttpStatus.INTERNAL_SERVER_ERROR);
+		}
+	}
 	
-	@GetMapping("/rooms")
+	@GetMapping("/hotel/rooms")
 	public List<Room> getRooms() {
 		return this.roomService.getRooms();
 	}
 
-	@GetMapping("/room/{room_id}")
+	@GetMapping("/hotel/room/{room_id}")
 	public Room getRoom(@PathVariable int room_id) {
 		return this.roomService.getRoom(room_id);
 	}
 	
-	@PostMapping("/rooms")
+	@PostMapping("/hotel/room")
 	public Room addRoom(@RequestBody Room room) {
 		return this.roomService.addRoom(room);
 	}
 
-	@PutMapping("/rooms")
+	@PutMapping("/hotel/room")
 	public Room updateRoom(@RequestBody Room room) {
 		return this.roomService.updateRoom(room);
 	}
 	
 	
-	@DeleteMapping("/room/{room_id}")
+	@DeleteMapping("/hotel/room/{room_id}")
 	public ResponseEntity<HttpStatus> deleteRoom(@PathVariable String id) {
 		try {
 			
@@ -56,4 +97,6 @@ public class RoomController {
 		}
 	}
 	
+	
+
 }
