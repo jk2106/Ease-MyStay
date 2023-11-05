0ab0ec23f69e555e675e72f556e4f7fa0a69b24e
ashish dash <1941017024.a.ashishdash@gmail.com>
2023-11-05 18:46:02 +0530

    Hotel HotelOwner Login controllers added !

--- EaseMyStay/src/main/java/com/fil/easemystay/controller/HotelOwnerController.java
+++ EaseMyStay/src/main/java/com/fil/easemystay/controller/HotelOwnerController.java
@@ -1,3 +1,5 @@
+
+
 package com.fil.easemystay.controller;
 
 import java.util.List;
@@ -6,66 +8,104 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.fil.easemystay.entities.Admin;
 import com.fil.easemystay.entities.Hotel;
 import com.fil.easemystay.entities.HotelOwner;
+import com.fil.easemystay.entities.Room;
 import com.fil.easemystay.service.HotelOwnerService;
 
-@RequestMapping("api/v1/")
-@RestController
+import jakarta.servlet.http.HttpSession;
+
+//@RequestMapping("api/v1/")
+@Controller
 public class HotelOwnerController {
 
 	@Autowired
 	private HotelOwnerService hotelOwnerService;
 
-
-	@GetMapping("/homeOwner")
-//	@ResponseBody
-	public String home() {
-		System.out.println("inside HotelOwner home controller");
-		return "index"; //when we don't write ResponseBody, we need to specify te
-	}
-
 	@GetMapping("/hotelOwners")
-	public List<HotelOwner> getHotelOwners() {
+	public String getHotelOwners(Model model) {
 		System.out.println("inside get");
-		return this.hotelOwnerService.getHotelOwners();
+		List<HotelOwner> owners = hotelOwnerService.getHotelOwners();
+		model.addAttribute("owners",owners );
+		System.out.println(owners);
+		return "ownerList";
 	}
 
-	@GetMapping("/hotelOwner/{id}")
-	public HotelOwner getHotel(@PathVariable int id) {
-		return this.hotelOwnerService.getHotelOwner(id);
-	}
-
-	@PostMapping("/hotelOwner")
-	public HotelOwner addHotel(@RequestBody HotelOwner hotelOwner) {
-		System.out.println("inside post");
-		return this.hotelOwnerService.addHotelOwner(hotelOwner);
-	}
-
-	@PutMapping("/hotelOwner")
-	public HotelOwner updateHotel(@RequestBody HotelOwner hotelOwner) {
-		return this.hotelOwnerService.updateHotelOwner(hotelOwner);
-	}
-
-	@DeleteMapping("/hotelOwner/{hotelOwner_id}")
-	public ResponseEntity<HttpStatus> deleteHotelOwner(@PathVariable int id) {
-		try {
-			this.hotelOwnerService.removeHotelOwner(id);
-			return new ResponseEntity<>(HttpStatus.OK);
-		} catch (Exception e) {
-			return new ResponseEntity<>(HttpStatus.INTERNAL_SERVER_ERROR);
+	// add new owner:
+		@GetMapping("/hotelOwners/signup")
+		public String addOwnerForm(Model model) {
+			model.addAttribute("owner", new HotelOwner());
+			return "ownerSignUp";
 		}
-	}
 
+		@PostMapping("/hotelOwners/signup")
+		public String addHotelOwner(@ModelAttribute("hotelOwner") HotelOwner newHotelOwner) {
+			System.out.println("inside post");
+			this.hotelOwnerService.addHotelOwner(newHotelOwner);
+			// model.addAttribute("hotel", newHotel);
+			return "redirect:/hotelOwners/login";
+		}
+		
+		@GetMapping("/hotelOwners/delete/{id}")
+		public String deleteHotelOwner(@PathVariable int id) {
+			try {
+				this.hotelOwnerService.removeHotelOwner(id);
+				return "redirect:/hotelOwners";
+			} catch (Exception e) {
+				return "redirect:/hotelOwners";
+			}
+		}
+		
+		@GetMapping("hotelOwners/login")
+		public String showLoginForm(Model model) {
+			model.addAttribute("owner", new HotelOwner());
+			return "ownerlogin";
+		}
+		@PostMapping("hotelOwners/login")
+		public String loginHotelOwner(HotelOwner  owner, Model model) throws Exception {
+	      String email = owner.getEmail();
+	      String password = owner.getPassword();
+	   
+//	      System.out.println("owner_id"+ owner.getOwner_id());
+	      System.out.println(email+"-----------------------");
+
+	      HotelOwner authenticatedOwner = this.hotelOwnerService.loginOwner(email, password);
+
+	      if (authenticatedOwner != null) {
+	    	  System.out.println("valid owner");
+	    	  return "redirect:/hotelByEmail/"+email;
+	      } else {
+	    	  model.addAttribute("errorMessage", "Invalid credentials. Please try again.");
+	    	  System.out.println("not a valid owner");
+	          return "ownerlogin";
+	      }
+		}
+		
+		
+		  @GetMapping("hotelOwners/logout")
+		  public ResponseEntity<String> logout(HttpSession session) {
+		      session.invalidate();
+		      String alertScript = "<script>alert('Logged out successfully');</script>";
+		      
+		      // You can redirect to a URL that triggers the JavaScript alert
+		      // Or, return a script to the client to display the alert
+		      return ResponseEntity.status(HttpStatus.OK).body(alertScript);
+		      
+		      }
+
+			
 }
-	
