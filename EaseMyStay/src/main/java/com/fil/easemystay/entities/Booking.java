b42e8fc414de42dddcfb50e95efcd14f1c207bd0
parth <parthsharma9109@gmail.com>
2023-11-05 18:22:35 +0530

    Booking code

--- EaseMyStay/src/main/java/com/fil/easemystay/entities/Booking.java
+++ EaseMyStay/src/main/java/com/fil/easemystay/entities/Booking.java
@@ -1,79 +1,112 @@
 package com.fil.easemystay.entities;
 
+import java.util.List;
+
+import jakarta.persistence.CascadeType;
+import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.OneToMany;
+
+@Entity
 public class Booking {
-	 private int booking_id;
-	    private int room_id;
-	    private int user_id;
-	    private int payment_id;
-	    private String check_in;
-	    private String check_out;
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private int booking_id;
+	private int payment_id;
+	private String check_in;
+	private String check_out;
 
-	    // Constructors
-	    public Booking() {
-	        // Default constructor
-	    }
+	@ManyToOne(cascade = CascadeType.ALL, fetch = FetchType.EAGER)
+	User user;
 
-	    public Booking(int booking_id, int room_id, int user_id, int payment_id, String check_in, String check_out) {
-	        this.booking_id = booking_id;
-	        this.room_id = room_id;
-	        this.user_id = user_id;
-	        this.payment_id = payment_id;
-	        this.check_in = check_in;
-	        this.check_out = check_out;
-	    }
-	 // Getter and Setter methods for each attribute
-	    public int getBooking_id() {
-	        return booking_id;
-	    }
+	@ManyToOne(cascade = CascadeType.ALL, fetch = FetchType.EAGER)
+	Hotel hotel;
 
-	    public void setBooking_id(int booking_id) {
-	        this.booking_id = booking_id;
-	    }
+	@ManyToOne(cascade = CascadeType.ALL, fetch = FetchType.EAGER)
+	Room room;
 
-	    public int getRoom_id() {
-	        return room_id;
-	    }
+	public User getUser() {
+		return user;
+	}
 
-	    public void setRoom_id(int room_id) {
-	        this.room_id = room_id;
-	    }
+	public void setUser(User user) {
+		this.user = user;
+	}
 
-	    public int getUser_id() {
-	        return user_id;
-	    }
-	    public void setUser_id(int user_id) {
-	        this.user_id = user_id;
-	    }
+	public Hotel getHotel() {
+		return hotel;
+	}
 
-	    public int getPayment_id() {
-	        return payment_id;
-	    }
+	public void setHotel(Hotel hotel) {
+		this.hotel = hotel;
+	}
 
-	    public void setPayment_id(int payment_id) {
-	        this.payment_id = payment_id;
-	    }
+	public Room getRoom() {
+		return room;
+	}
 
-	    public String getCheck_in() {
-	        return check_in;
-	    }
+	public void setRoom(Room room) {
+		this.room = room;
+	}
 
-	    public void setCheck_in(String check_in) {
-	        this.check_in = check_in;
-	    }
+	// Getter and Setter methods for each attribute
+	public int getBooking_id() {
+		return booking_id;
+	}
 
-	    public String getCheck_out() {
-	        return check_out;
-	    }
+	public void setBooking_id(int booking_id) {
+		this.booking_id = booking_id;
+	}
 
+	public int getPayment_id() {
+		return payment_id;
+	}
 
-	    public void setCheck_out(String check_out) {
-	        this.check_out = check_out;
-	    }
+	public void setPayment_id(int payment_id) {
+		this.payment_id = payment_id;
+	}
 
-		@Override
-		public String toString() {
-			return "Booking [booking_id=" + booking_id + ", room_id=" + room_id + ", user_id=" + user_id
-					+ ", payment_id=" + payment_id + ", check_in=" + check_in + ", check_out=" + check_out + "]";
-		}
+	public String getCheck_in() {
+		return check_in;
+	}
+
+	public void setCheck_in(String check_in) {
+		this.check_in = check_in;
+	}
+
+	public String getCheck_out() {
+		return check_out;
+	}
+
+	public void setCheck_out(String check_out) {
+		this.check_out = check_out;
+	}
+
+	// Constructors
+	public Booking() {
+		// Default constructor
+	}
+
+	public Booking(int booking_id, int payment_id, String check_in, String check_out, User user, Hotel hotel,
+			Room room) {
+		super();
+		this.booking_id = booking_id;
+		this.payment_id = payment_id;
+		this.check_in = check_in;
+		this.check_out = check_out;
+		this.user = user;
+		this.hotel = hotel;
+		this.room = room;
+	}
+
+	@Override
+	public String toString() {
+		return "Booking [booking_id=" + booking_id + ", payment_id=" + payment_id + ", check_in=" + check_in
+				+ ", check_out=" + check_out + ", user=" + user + ", hotel=" + hotel + ", room=" + room + "]";
+	}
 
 }
