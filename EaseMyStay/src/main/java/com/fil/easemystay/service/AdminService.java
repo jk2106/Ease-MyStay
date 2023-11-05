e9c25c3fea6bc9b19dda1e899567b7f5d1ce3994
Harshita-Kohli <54809528+Harshita-Kohli@users.noreply.github.com>
2023-11-05 17:15:24 +0530

    committing admin service and service impl

--- EaseMyStay/src/main/java/com/fil/easemystay/service/AdminService.java
+++ EaseMyStay/src/main/java/com/fil/easemystay/service/AdminService.java
@@ -1,9 +1,10 @@
 package com.fil.easemystay.service;
 
 import com.fil.easemystay.entities.Admin;
+import com.fil.easemystay.entities.User;
 
 public interface AdminService {
-	
-	public Admin registerAdmin(Admin admin) throws Exception;
+
+	public Admin loginAdmin(String email, String password);
 
 }
